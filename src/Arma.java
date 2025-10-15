36926e8ef12cf8be1c303de53560692d008354ed
Djulian858 <velasquezjulian03@gmail.com>
2025-10-14 20:02:10 -0500

    SEagrega la clase arma

---
+++ src/Arma.java
@@ -0,0 +1,28 @@
+public class Arma {
+    private String nombre;
+    private int dañoAdicional;
+
+    public Arma(String nombre, int dañoAdicional) {
+        this.nombre = nombre;
+        this.dañoAdicional = dañoAdicional;
+    }
+
+    public void atacarConArma(Criatura objetivo) {
+        int dañoTotal = dañoAdicional;
+        objetivo.defender(dañoTotal);
+        System.out.println("¡Ataque con " + nombre + " infligiendo " + dañoTotal + " de daño adicional!");
+    }
+
+    public int getDañoAdicional() {
+        return dañoAdicional;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (+" + dañoAdicional + " daño)";
+    }
+}
